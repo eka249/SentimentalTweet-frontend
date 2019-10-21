fe4f6cc0cf8f4f388612f18df531657fb9300172
Ellyn <eka249@nyu.edu>
2019-10-21 14:40:59 -0700

    began modal

--- sent-tweet-frontend/src/App.js
+++ sent-tweet-frontend/src/App.js
@@ -1,66 +1,20 @@
 import React from "react";
-import SearchBar from "./SearchBar";
-
-var Twitter = require("twitter");
-var json = require("../src/api/config.json");
+import NavBar from "./containers/NavBar";
+import SearchHome from "./containers/SearchHome";
+import Tweets from "./containers/Tweets";
+import SignIn from "./components/SignIn";
 
 class App extends React.Component {
-  state = {
-    tweets: []
-    // ,term: ""
-  };
-
-  // onSearchSubmit = term => {
-  //   this.setState({ term });
-  //   console.log("Term:", term);
-  // };
-
-  searchTwitter = term => {
-    // console.log("hit");
-    // this.setState({ term });
-    // console.log("passed through state", term);
-    let client = new Twitter({
-      consumer_key: json.consumer_key,
-      consumer_secret: json.consumer_secret,
-      access_token_key: json.access_token_key,
-      access_token_secret: json.access_token_secret
-    });
-
-    let params = {
-      screen_name: term
-    };
-
-    client.get(
-      // "https://api.twitter.com/1.1/users/lookup.json",
-      "statuses/user_timeline",
-      params,
-      function(error, tweets, response) {
-        console.log(response.body);
-        // response.writeHead(200, { "Content-Type": "application/json" });
-        // response.end(JSON.stringify(response.body)); // This line sends the tweets to the client making the http request.
-      }
-    );
-  };
-
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <NavBar />
+        {/* <NavBar />
         <SearchHome />
-        <Tweets />
+        <Tweets /> */}
+        <SignIn />
       </div>
     );
   }
 }
-
-// http.createServer(this.searchTwitter).listen(PORT);
-
-// app.listen(PORT, function() {
-//   console.log("Server is running on PORT:", PORT);
-// });
-
-// searchTwitter.listen(3001, function() {
-//   console.log("Example app listening on poor 3001!");
-// });
 
 export default App;
