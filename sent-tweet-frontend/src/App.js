69e1afac14dde79fc5864b0ec18cf184017440e7
Catherine Liahn Yang <liahnee@gmail.com>
2019-10-21 17:02:00 -0700

    navbar

--- sent-tweet-frontend/src/App.js
+++ sent-tweet-frontend/src/App.js
@@ -3,13 +3,22 @@ import NavBar from "./containers/NavBar";
 import SearchHome from "./containers/SearchHome";
 import Tweets from "./containers/Tweets";
 import SignInTestButton from "./components/SignIn";
+import 'semantic-ui-css/semantic.min.css';
 
 class App extends React.Component {
+  state = {
+    loggedin: false,
+    displayName: "", //user's displayName
+    favorites: [], //user's list of fav
+    tweets: [], //tweets of selectedAcc 
+    selectedAcc: [] //twitteraccount
+  };
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        {/* <NavBar />
-        <SearchHome />
+        <NavBar favorites={this.state.favorites} />
+        {/* <SearchHome />
         <Tweets /> */}
         <SignInTestButton />
       </div>
